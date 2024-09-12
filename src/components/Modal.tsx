a5f19133d82cb3064ba544c54f8dd0fb8e2796f1
IraisValenzuela <iraisvq8@gmail.com>
2024-09-12 16:37:44 -0700

    modal logic

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,39 +1,74 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 interface CustomHolidayProps {
   showModal: boolean;
-  setShowModal: (value: boolean) => void
+  setShowModal: (value: boolean) => void;
 }
+interface CustomHoliday {
+  name: string;
+  date: string;
+}
+const CustomHolidayModal = ({
+  showModal,
+  setShowModal,
+}: CustomHolidayProps) => {
+  const [customHolidayData, setCustomHolidayData] = useState<CustomHoliday>({
+    name: "",
+    date: "",
+  });
 
-const CustomHolidayModal = ({showModal, setShowModal}: CustomHolidayProps) => {
-
-  const handleClose = () => {
+  const handleClose = (e: React.FormEvent) => {
+    e.preventDefault();
     setShowModal(false);
-  }
+    setCustomHolidayData({
+      name: "",
+      date: "",
+    });
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCustomHolidayData({
+      ...customHolidayData,
+      [name]: value,
+    });
+  };
 
   return (
     <>
-      <Modal show={showModal} onHide={handleClose}>
+      <Modal show={showModal}>
         <Modal.Header closeButton>
           <Modal.Title>Add your holiday</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Please Provide the Name of the holiday and date</Modal.Body>
-        <input type='text' placeholder='Holiday Name'></input>
-        <input type='date' />
-
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Modal.Body>Please Provide the name of the holiday and date</Modal.Body>
+        <form onSubmit={handleClose}>
+          <input
+            type="text"
+            placeholder="Holiday Name"
+            value={customHolidayData.name}
+            name="name"
+            onChange={handleChange}
+          ></input>
+          <input
+            type="date"
+            name="date"
+            value={customHolidayData.date}
+            onChange={handleChange}
+          />
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" type="submit" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </form>
       </Modal>
     </>
   );
-}
+};
 
 export default CustomHolidayModal;
